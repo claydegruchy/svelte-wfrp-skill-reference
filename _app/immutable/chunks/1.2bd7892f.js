11dfbaa887008834ac100ac7ff057bb8c7f380ab
clay <clay.degruchy+github@gmail.com>
2023-12-02 15:35:24 +0100

    Updates

--- _app/immutable/chunks/1.2bd7892f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.10df40eb.js";export{t as component};
---
+++ _app/immutable/chunks/1.e0db2977.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.c88a928e.js";export{t as component};
